880c840d204373e3be9258619a7cad190d9cf124
yasith <yasithmadu@gmail.com>
2023-02-20 00:40:33 +0530

    Initial commit

---
+++ test/meal_planner.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MealPlanner from '../lib/meal_planner-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/meal_planner-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MealPlanner.MealPlannerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
